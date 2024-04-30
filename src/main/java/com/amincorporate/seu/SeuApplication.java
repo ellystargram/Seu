9fb7a8f21052130575d0093d38b69185f19ac0f0
Heeyeon Lee <up2kid@gmail.com>
2024-04-30 16:53:58 +0900

    Initial Commit

---
+++ src/main/java/com/amincorporate/seu/SeuApplication.java
@@ -0,0 +1,13 @@
+package com.amincorporate.seu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amincorporate/seu/SeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amincorporate.seu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
